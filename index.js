fd8415f532d5e7538050060c3e3b44ca546c284a
JENNIFER974-DEL <jennifernyaguthii23@gmail.com>
2024-12-13 05:45:09 +0300

    done lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+
+  function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  function fuzzyMatch(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(name.toLowerCase()));
+  }
+  
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
+  module.exports = { findMatching, fuzzyMatch, matchName }; 
+    
